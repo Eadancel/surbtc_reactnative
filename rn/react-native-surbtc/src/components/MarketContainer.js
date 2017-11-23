4415aa0f86de6ad021842e1e002337d3c0a728e4
Leopoldo Rosas <polo@Leopoldos-MacBook.local>
2017-11-22 23:02:00 -0300

    Mocked values formating values

--- rn/react-native-surbtc/src/components/MarketContainer.js
+++ rn/react-native-surbtc/src/components/MarketContainer.js
@@ -2,14 +2,26 @@ import  React, {Component}  from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import { Header, Ticker, Trades, TextTrades } from './';
 
+var MOCKED_SURBTC_TICKER = [
+	{"ticker":{"last_price":["5348000.0","CLP"],
+			   "min_ask":["5348000.0","CLP"],
+			   "max_bid":["5305015.01","CLP"],
+			   "volume":["31.82230733","BTC"],
+			   "price_variation_24h":"-0.006",
+			   "price_variation_7d":"0.129"}
+			},
+  ];
+var numeral = require ('numeral');
 
 class MarketContainer extends Component {
 	render(){
 		const {marketID} = this.props;
+		const {ticker} = MOCKED_SURBTC_TICKER[0];
+
 		return (
 			<View style={marketContainer}>
 				<Header marketID={marketID}/>
-				<Ticker ticker_value={"4.4M"}/>
+				<Ticker ticker={ticker['last_price'][0]} variation={ticker['price_variation_24h']}/>
 				<Trades buys={"12"} sales={"33"}/>
 			</View>
 		)
--- rn/react-native-surbtc/src/components/Ticker.js
+++ rn/react-native-surbtc/src/components/Ticker.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+var numeral = require ('numeral');
 
-const Ticker = ({ticker_value}) => {
+
+const Ticker = ({ticker, variation}) => {
+	var ticker_value = parseInt(ticker);
+	var variation_value = parseFloat(variation);
+
+	var ticker_text = numeral(ticker_value).format('$ 0.0 a');
+	var variation_text = numeral(variation_value).format('0.00%');
 	return (
 		<View style={tickerContainer}>
 			<Text style={tickerText}>
-				{ticker_value}
+				{ticker_text}
+			</Text>
+			<Text style={variationText}>
+				{variation_text}
 			</Text>
 		</View>
 	)
@@ -22,9 +32,14 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 		fontSize : 100,
 		color : "#00ff00"
+	},
+	variationText : {
+		fontWeight: "bold",
+		fontSize : 40,
+		color : "#00ff00"
 	}
 })
 
-const { tickerContainer, tickerText } = styles;
+const { tickerContainer, tickerText, variationText } = styles;
 
 export default Ticker
